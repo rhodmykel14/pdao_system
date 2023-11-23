ba8a5eb031999f4688e343a849978fdd0140768b
rhodmykel14 <147973368+rhodmykel14@users.noreply.github.com>
2023-11-23 16:19:53 +0800

    Add files via upload

--- controller/reportsController.php
+++ controller/reportsController.php
@@ -1,16 +1,58 @@
 <?php
 include('connection/connection.php');
 
+$reportType = $_POST['reportType'];
 
-$sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate FROM pwd ORDER BY birthDate ASC;"
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    //unsure if $sql var should be accessible to all conditions,,,
+    $sql = "";
 
-$sql = "SELECT * FROM pwd ORDER BY dateApplied ASC;"
+    //report to view pwd essential info, sorted by age
+    if ($reportType == '1') {
+                                    $sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd ORDER BY birthDate ASC;"; echo $sql;
+                            }
 
-$sql =    "SELECT * FROM pwd WHERE educationalAttainment = ;"
-$sql =    "SELECT * FROM pwd WHERE employmentStatus = ;"
-$sql =    "SELECT * FROM pwd WHERE employmentCategory = ;"
-$sql =    "SELECT * FROM pwd WHERE employmentType = ;"
-$sql =    "SELECT * FROM pwd WHERE occupation = ;"
+    //report, view pwd info by most recent applicant
+    elseif ($reportType == '2') {
+                                    $sql = "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd ORDER BY dateApplied ASC;"; echo $sql;
+                                }
 
-$sql =    "SELECT * FROM pwd WHERE orgAffiliated = ;"
+    //report, view pwd info in terms of educational and employment attainment
+    //add something after the = ? variable realness
+    elseif ($reportType == '3') {
+                                    $educationalAttainment = $_POST['educationalAttainment'];
+                                    $sql = "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd WHERE educationalAttainment = '{$educationalAttainment}'"; echo $sql;
+                                }
+    elseif ($reportType == '4') {
+                                    $employmentStatus = $_POST['employmentStatus'];
+                                    $sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd WHERE employmentStatus = '{$employmentStatus}'"; echo $sql;
+                                }
+    elseif ($reportType == '5') {
+                                    $employmentCategory = $_POST['employmentCategory'];
+                                    $sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd WHERE employmentCategory = '{$employmentCategory}'"; echo $sql;
+                                }
+    elseif ($reportType == '6') {
+                                    $employmentType = $_POST['employmentType'];
+                                    $sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd WHERE employmentType = '{$employmentType}'"; echo $sql;
+                                }
+    elseif ($reportType == '7') { 
+                                    $occupation = $_POST['occupation'];
+                                    $sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd WHERE occupation = '{$occupation}'"; echo $sql;
+                                }
+
+    //report, view pwd info re: org in charge of their registration
+    elseif ($reportType == '8') {
+                                    $orgAffiliated = $_POST['orgAffiliated'];
+                                    $sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate 
+                                    FROM pwd WHERE orgAffiliated = '{$orgAffiliated}'"; echo $sql;
+                                }
+}
 ?>

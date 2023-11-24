ed4459618418c215ccf8476812039a0dfef27ed0
rhodmykel14 <147973368+rhodmykel14@users.noreply.github.com>
2023-11-24 16:12:38 +0800

    Add files via upload

--- dashboard/reports.php
+++ dashboard/reports.php
@@ -1,18 +1,152 @@
 <?php
 
-include('connection/connection.php');
+require_once('../controller/connection/connection.php');
 
+$result = mysqli_query($conn,$query);
 
-$sql =  "SELECT id, pwdNumber, lastName, firstName, middleName, birthDate FROM pwd ORDER BY birthDate ASC;"
-
-$sql = "SELECT * FROM pwd ORDER BY dateApplied ASC;"
-
-$sql =    "SELECT * FROM pwd WHERE educationalAttainment = ;"
-$sql =    "SELECT * FROM pwd WHERE employmentStatus = ;"
-$sql =    "SELECT * FROM pwd WHERE employmentCategory = ;"
-$sql =    "SELECT * FROM pwd WHERE employmentType = ;"
-$sql =    "SELECT * FROM pwd WHERE occupation = ;"
-
-$sql =    "SELECT * FROM pwd WHERE orgAffiliated = ;"
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Now UI Dashboard by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="blue">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+           
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          PDAO SYSTEM<br>
+          <span style="margin-left:15%">Admin</span>
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li class="">
+            <a href="./dashboard.php">
+              <i class="now-ui-icons design_app"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li class="dropdown-btn 1 active">
+            <a href="./user.php">
+              <i class="now-ui-icons users_single-02"></i>
+              <p>Users</p>
+            </a>
+          </li>
+          <li class="dropdown-menu-md-right">
+            <a style="margin-bottom:3% !important;" href="./benefits.php">
+            <i class="now-ui-icons location_map-big dropdown-menu-md-right"></i>
+            <p>Benefits </p></a>
+          </li>
+          <li class="dropdown-menu-md-right">
+            <a style="margin-bottom:3% !important;" href="./announcement.php">
+              <i class="now-ui-icons ui-1_bell-53"></i>
+              <p>Announcement</p>
+            </a>
+          </li>
+          <li class="dropdown-menu-md-right">
+            <a style="margin-bottom:3% !important;" href="./reports.php">
+              <i class="now-ui-icons files_single-copy-04 dropdown-menu-md-right"></i>
+              <p>Generate Report </p>
+            </a>
+          </li>
+          <li class="dropdown-menu-md-right">
+            <a href="./register.php" style="margin-bottom:3% !important;">
+              <i class="now-ui-icons now-ui-icons tech_laptop"></i>
+              <p>Register</p>
+            </a>
+          </li>
+
+          <li class="active-pro">
+            <a href="./upgrade.html">
+              <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+              <p>Upgrade to PRO</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel" id="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" href="#pablo">Reports List</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navigation">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#pablo">
+                  <i class="now-ui-icons media-2_sound-wave"></i>
+                  <p>
+                    <span class="d-lg-none d-md-block">Stats</span>
+                  </p>
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="now-ui-icons location_world"></i>
+                  <p>
+                    <span class="d-lg-none d-md-block">Some Actions</span>
+                  </p>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#pablo">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>
+                    <span class="d-lg-none d-md-block">Account</span>
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+     <!-- End Navbar -->
+<!-- things to add: structure of reports selection menu, 
+input fields when reports need more information, and refresh page on query fetch-->
+
+

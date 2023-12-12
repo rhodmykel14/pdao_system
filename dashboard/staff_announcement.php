f9fd1046b3d7edf6ccd25fef56659a97bf0eb0b0
rhodmykel14 <147973368+rhodmykel14@users.noreply.github.com>
2023-12-12 10:39:11 +0800

    Add files via upload

--- dashboard/staff_announcement.php
+++ dashboard/staff_announcement.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-$_SESSION['username'] = ['pwdadmin', 'rm.adalid'];
-$_SESSION['userType'] = ['admin'];
+$_SESSION['username'] = ['pwdstaff'];
+$_SESSION['userType'] = ['staff'];
 
 require_once('../controller/connection/connection.php');
 
@@ -51,46 +51,25 @@ $result = mysqli_query($conn,$query);
         </a>
         <a href="http://www.creative-tim.com" class="simple-text logo-normal">
           PDAO SYSTEM<br>
-          <span style="margin-left:15%">Admin</span>
+          <span style="margin-left:15%">Staff</span>
         </a>
       </div>
       <div class="sidebar-wrapper" id="sidebar-wrapper">
         <ul class="nav">
-          <li class="">
-            <a href="./dashboard.php">
-              <i class="now-ui-icons design_app"></i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li class="dropdown-btn 1 ">
-            <a href="./user.php">
+          <li class="dropdown-btn 1">
+            <a href="./staff_pwdUser.php">
               <i class="now-ui-icons users_single-02"></i>
-              <p>Users</p>
+              <p>PWD Users</p>
             </a>
           </li>
-          <li class="dropdown-menu-md-right">
-            <a style="margin-bottom:3% !important;" href="./benefits.php">
-            <i class="now-ui-icons location_map-big dropdown-menu-md-right"></i>
-            <p>Benefits </p></a>
-          </li>
-          <li class="dropdown-menu-md-right active" >
-            <a style="margin-bottom:3% !important;" href="./announcement.php">
+          <li class="dropdown-menu-md-right active">
+            <a style="margin-bottom:3% !important;" href="./staff_announcement.php">
               <i class="now-ui-icons ui-1_bell-53"></i>
               <p>Announcement</p>
             </a>
           </li>
-          <li class="dropdown-menu-md-right">
-            <a style="margin-bottom:3% !important;" href="./reports.php">
-              <i class="now-ui-icons files_single-copy-04 dropdown-menu-md-right"></i>
-              <p>Generate Report </p>
-            </a>
-          </li>
-          <li class="dropdown-menu-md-right ">
-            <a href="./registerForStaff.php" style="margin-bottom:3% !important;">
-              <i class="now-ui-icons now-ui-icons tech_laptop"></i>
-              <p>Staff Registration</p>
-            </a>
-          </li>
+
+
           <li class="active-pro">
             <a href="./upgrade.html">
               <i class="now-ui-icons arrows-1_cloud-download-93"></i>
@@ -169,7 +148,7 @@ $result = mysqli_query($conn,$query);
               <div class="card-body">
                 <div class="panel panel-default">
                   <div class="panel-body">
-                    <form action="../controller/announcementController.php" method="POST" enctype="multipart/form-data">
+                    <form action="../controller/announcementControllerStaff.php" method="POST" enctype="multipart/form-data">
                       <div class="row">
                         <div class="col-md-6 pr-1">
                           <div class="form-group">
@@ -218,46 +197,61 @@ $result = mysqli_query($conn,$query);
 
               </div>
               <div class="card-body">
-                <div class="table-responsive">
+              <div class="table-responsive">
                   <table class="table">
-                        <thead class=" text-primary">
-                        <tr>
-                          <td>Event Name</td>
-                          <td>Event Date</td>
-                          <td>Description</td>
-                          <td>Event Place</td>
-                          <td>Action</td>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <?php
-                          $query_run = mysqli_query($conn, $query);
+                    <thead class=" text-primary">
+                      <th>
+                        Event Name
+                      </th>
+                      <th>
+                        Event Date
+                      </th>
+                      <th>
+                        Event Description
+                      </th>
+                      <th>
+                        Event Place
+                      </th>
+                      <th>
+                        Action 
+                      </th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                      <?php
 
-                          if(mysqli_num_rows($query_run) > 0)
-                          {
-                            foreach($query_run as $announcement)
+                      while($row = mysqli_fetch_assoc($result))
                             {
-                                //echo $pwd[]
-                                ?>
-                                <tr>
-                                  <td><?= $announcement['announcementName']; ?></td>
-                                  <td><?= $announcement['announcementDate']; ?></td>
-                                  <td><?= $announcement['announcementDesc']; ?></td>
-                                  <td><?= $announcement['announcementPlace']; ?></td>
-                                  <td>
-                                    <a href="staff_editPWD.php?<?= $pwd['id']?>" class="btn btn-success btn-sm">Edit</a>
-                                    <a href="" class="btn btn-danger btn-sm">Delete</a>
-                                  </td>
-                                </tr>
-                                <?php
+                      ?>
+                        <td>
+                          <?php echo $row['announcementName']; ?>
+                        </td>
+                        <td>
+                          <?php echo $row['announcementDate']; ?>
+                        </td>
+                        <td>
+                          <?php echo $row['announcementDesc']; ?>
+                        </td>
+                        <td>
+                          <?php echo $row['announcementPlace']; ?>
+                        </td>
+                        <td class="td-actions">
+                        <form action="../controller/deleteAnnouncementController.php" method="post">
+                          <button type="button" rel="tooltip" class="btn btn-success btn-sm btn-icon">
+                              <i class="now-ui-icons ui-2_settings-90"></i>
+                          </button>
+                            <input type="hidden" name="announcement_id" value="<?php echo $row['id']; ?>">
+                            <button type="submit" rel="tooltip" class="btn btn-danger btn-sm btn-icon" onclick="return confirm('Are you sure you want to delete this announcement?');">
+                                <i class="now-ui-icons ui-1_simple-remove"></i>
+                            </button>
+                          </form>
+                        </td>
+                      </tr>
+                      <?php
                             }
-                          }
-                          else
-                          {
-                            echo "<h5> No Record Found. </h5>";
-                          }
-                          ?>
-                      </tbody> 
+                      ?>
+                      
+                    </tbody>
                   </table>
                 </div>
               </div>
